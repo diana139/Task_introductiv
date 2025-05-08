edb0724884169b41b14ad85732e0c3871a4a7b90
diana139 <dianarosuu18@gmail.com>
2025-05-08 20:31:55 +0300

    Update page.tsx

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>homeee dffffffff</div>
+   <div>homeee dffffffff
+    dcwffwg
+    weftrgbre
+    er
+    g
+   </div>
   );
 }
