ee92a6d283c2e7203e4033ece5359b28f6ac2d89
diana139 <dianarosuu18@gmail.com>
2025-05-09 12:30:51 +0300

    .

---
+++ site/app/components/navbar.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <>
+        <nav style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "10px 10px",
+            backgroundColor: "#ffffff",
+            borderBottom: "1px solid #ccc"
+        }}>
+            
+            <Link href="/">
+                <Image 
+                    src="https://www.asii.ro/_astro/asii.e9f13d98.svg" 
+                    height={100} 
+                    width={100} 
+                    alt="logo" 
+                    style={{ cursor: "pointer" }}
+                />
+            </Link>
+
+            <ul className="text-black font-bold flex gap-10 text-[50px]" style={{
+                display: "flex",
+                gap: "30px",
+                listStyle: "none",
+                margin: 0,
+                padding: 0,
+            }} >
+                <li><Link href="/">Acasa</Link></li>
+                <li><Link href="/mentori">Mentori</Link></li>
+                <li><Link href="/boboci">Boboci</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+        </>
+        
+        
+        
+    );
+}
